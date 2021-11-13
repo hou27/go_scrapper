eaa15a487ac915960abd4550d9376ea072977901
hou27 <ataj125@gmail.com>
2021-11-13 10:51:38 +0000

    channel 2

--- main.go
+++ main.go
@@ -7,18 +7,17 @@ import (
 
 func main() {
 	// channel
-	c := make(chan bool)
+	c := make(chan string)
 	people := [2]string{"jalapeno", "hou27"}
 	for _, person := range people {
 		go isSexy(person, c)
 	}
-	fmt.Println(<-c) // main func wait until reply
-	fmt.Println(<-c)
-	// fmt.Println(<-c)
+	for i := 0; i < len(people); i++ {
+		fmt.Println(<-c) // main func wait until reply(receiving is blocking operation)
+	}
 }
 
-func isSexy(person string, c chan bool) {
-	time.Sleep(time.Second * 3)
-	fmt.Println(person)
-	c <- true // send true to channel
+func isSexy(person string, c chan string) {
+	time.Sleep(time.Second * 5)
+	c <- person + " is sexy" // send to channel
 }
