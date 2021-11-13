2ec800bed4178c584f67ebf87d21a1123e37e60b
hou27 <ataj125@gmail.com>
2021-11-13 03:46:29 +0000

    pointer receiver

--- accounts/accounts.go
+++ accounts/accounts.go
@@ -21,8 +21,15 @@ func NewAccount(owner string) *Account {
  *
  */
 
+/**
+ * Pointer Receiver
+ * 
+ * receiver의 값을 변경하고자 할 때(단순히 읽기가 아닌 쓰기 작업도 같이)
+ * struct의 크기가 커서 deep copy 비용이 클 때 
+ * 코드 일관성(option): 어떤 함수가 포인터 receiver를 사용하는 경우 일관성을 줄 때
+ */
 // Deposit x amount on your account
-func (a Account) Deposit(amount int) {
+func (a *Account) Deposit(amount int) { // use pointer receiver
 	a.balance += amount
 }
 
