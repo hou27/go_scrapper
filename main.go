e8a27b83d515a2c1d474f6e71d5d96fde2dd96d9
hou27 <ataj125@gmail.com>
2021-11-13 02:44:09 +0000

    variable expression

--- main.go
+++ main.go
@@ -2,15 +2,13 @@ package main
 
 import "fmt"
 
-func superAdd(numbers ...int) int {
-	total := 0
-	for /* index */ _, number := range numbers {
-		total += number
+func canIDrink(age int) bool {
+	if koreanAge := age + 2; koreanAge < 19 { // variable expression
+		return false
 	}
-	return total
+	return true
 }
 
 func main() {
-	result := superAdd(1, 2, 3, 4, 5, 6)
-	fmt.Println(result)
+	fmt.Println(canIDrink(16))
 }
