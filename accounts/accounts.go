0b6c4f94ba4a4de951725e813f5c5dd19227bd70
hou27 <ataj125@gmail.com>
2021-11-13 03:32:42 +0000

    another way

--- accounts/accounts.go
+++ accounts/accounts.go
@@ -12,3 +12,15 @@ func NewAccount(owner string) *Account {
 	account := Account{owner: owner, balance: 0}
 	return &account
 }
+
+type Thing struct {
+    Name  string
+    Num   int
+}
+
+func NewThing(someParameter string) *Thing {
+    p := new(Thing)
+    p.Name = someParameter
+    p.Num = 33 // <- a very sensible default value
+    return p
+}
