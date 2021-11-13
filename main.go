139355a039c722f747a805bdac0390b7c3130c3c
hou27 <ataj125@gmail.com>
2021-11-13 02:51:41 +0000

    switch

--- main.go
+++ main.go
@@ -3,10 +3,26 @@ package main
 import "fmt"
 
 func canIDrink(age int) bool {
-	if koreanAge := age + 2; koreanAge < 19 { // variable expression
+	// 1
+
+	// switch koreanAge := age + 2; koreanAge {
+	// case 10:
+	// 	return false
+	// case 19:
+	// 	return true
+	// }
+	// return false
+
+	// 2
+
+	koreanAge := age + 2;
+	switch true {
+	case koreanAge < 19:
 		return false
+	case koreanAge >= 19:
+		return true
 	}
-	return true
+	return false
 }
 
 func main() {
