f67940ebc44a1d7e7f0c4c2df9cc9d4ac2f94d94
hou27 <ataj125@gmail.com>
2021-11-14 05:02:42 +0000

    extractjobs

--- main.go
+++ main.go
@@ -6,12 +6,19 @@ import (
 	"log"
 	"net/http"
 	"strconv"
+	"strings"
 
 	"github.com/PuerkitoBio/goquery"
 )
 
-//https://stackoverflow.com/questions/58518588/vscode-could-not-import-golang-package
-//https://golang.org/doc/tutorial/create-module
+type extractedJob struct {
+	link     string
+	title    string
+	location string
+	salary   string
+	summary  string
+}
+
 type requestResult struct {
 	url    string
 	status string
@@ -24,13 +31,33 @@ func main() {
 	totalPages := getPages()
 
 	for i := 0; i < totalPages; i++ {
-		getPage(i)
+		jobs := getPage(i)
+		fmt.Println(jobs)
 	}
 }
 
-func getPage(page int) {
+func getPage(page int) []extractedJob {
+	var jobs []extractedJob
+
 	pageURL := baseURL + "&start=" + strconv.Itoa(page*50) // integer to ascii
 	fmt.Println("Requesting", pageURL)
+	res, err := http.Get(pageURL)
+	checkErr(err)
+	checkCode(res)
+
+	defer res.Body.Close()
+
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	checkErr(err)
+
+	searchCards := doc.Find(".tapItem")
+
+	searchCards.Each(func(i int, card *goquery.Selection) {
+		job := extractJob(card)
+		jobs = append(jobs, job)
+	})
+
+	return jobs
 }
 
 func getPages() int {
@@ -50,6 +77,29 @@ func getPages() int {
 	}) // func (*goquery.Selection).Each(f func(int, *goquery.Selection)) *goquery.Selection
 
 	return pages
+}
+
+func extractJob(card *goquery.Selection) extractedJob {
+	link, _ := card.Attr("href")
+		title:= card.Find(".jobTitle>span").Text()
+		location := card.Find(".companyLocation").Text()
+		salary := cleanString(card.Find(".salary-snippet").Text())
+		summary := card.Find(".job-snippet").Text()
+	return extractedJob{
+		link:     link,
+		title:    title,
+		location: location,
+		salary:   salary,
+		summary:  summary,
+	}
+}
+
+func cleanString(str string) string {
+	// clean the space from the sides, clean the gap between strings(return arr), concatenates the elements of its first argument to create a single string.
+	/**
+	FieldsFunc splits the string s at each run of Unicode code points c satisfying f(c) and returns an array of slices of s. If all code points in s satisfy f(c) or the string is empty, an empty slice is returned.
+	 */
+	return strings.Join(strings.Fields(strings.TrimSpace(str)), " ")
 }
 
 func checkErr(err error) {
